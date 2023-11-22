dcbc645b39295dce026b3d40e1aae99467745d77
francymora <79939741+francymora@users.noreply.github.com>
2023-11-22 22:41:55 +0000

    data change

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
-import express from 'express';
-const app = express();
+
 
 
 const footballSchema = new mongoose.Schema({
@@ -19,35 +18,3 @@ const FootballModel = mongoose.model('Football', footballSchema);
 export {FootballModel}
 
 
-// Assicurati che la connessione a MongoDB sia stabilita prima di eseguire le operazioni sul database
-/*
-// Esempio: Connessione a MongoDB
-mongoose.connect('mongodb://localhost:27017/Web_Application_Coursework')
-.then(() => {
-  console.log('Connessione al database MongoDB riuscita');
-
-  // Esempio di query per ottenere tutti i documenti dalla collezione "Football"
-  FootballModel.find({Team: 'Argentina'})
-    .then(results => {
-      console.log('Risultati della query:', results);
-    })
-    .catch(error => {
-      console.error('Errore nella query:', error);
-    });
-})
-.catch((error) => {
-  console.error('Errore di connessione al database:', error);
-});
-app.get('/football', async (req, res) => {
-  try {
-    const allFootballData = await FootballModel.find({}); // Query per ottenere tutti i documenti
-    res.json(allFootballData); // Invia i dati come JSON in risposta alla richiesta GET su /football
-  } catch (error) {
-    console.error('Errore nella query:', error);
-    res.status(500).json({ error: 'Errore nel recuperare i dati' }); // Invia un errore HTTP 500 in caso di errore
-  }
-});
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
-*/
